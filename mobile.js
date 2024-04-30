f4c01b1a6170bb22860167221a2e2d2933094d7a
luiscortez <felipecortez217@gmail.com>
2024-04-30 00:48:19 -0300

    att links

--- mobile.js
+++ mobile.js
@@ -1,9 +1,47 @@
 const menu = document.querySelector('#menu')
 const navMenu = document.querySelector('#nav-menu')
 const button = document.querySelector('#btn')
+const home = document.querySelector('.home')
+const sobre = document.querySelector('.sobre')
+const skils = document.querySelector('.skils')
+const projects = document.querySelector('.projects')
 
 menu.addEventListener('click',() => {
     menu.classList.toggle('ativo')
     navMenu.classList.toggle('ativo')
     button.classList.toggle('ativo')
 })
+
+home.addEventListener('click',() => {
+  menu.classList.toggle('ativo')
+  navMenu.classList.toggle('ativo')
+  button.classList.toggle('ativo')
+})
+
+sobre.addEventListener('click',() => {
+  menu.classList.toggle('ativo')
+  navMenu.classList.toggle('ativo')
+  button.classList.toggle('ativo')
+})
+
+skils.addEventListener('click',() => {
+  menu.classList.toggle('ativo')
+  navMenu.classList.toggle('ativo')
+  button.classList.toggle('ativo')
+})
+
+projects.addEventListener('click',() => {
+  menu.classList.toggle('ativo')
+  navMenu.classList.toggle('ativo')
+  button.classList.toggle('ativo')
+})
+
+document.querySelectorAll('.smooth-scroll').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
